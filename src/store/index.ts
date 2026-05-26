9b84252fcd3dd5f5cf8a4b4b50a5ce0775794566
jozefff1 <100238347+jozefff1@users.noreply.github.com>
2026-05-27 01:38:03 +0200

    chore: remove unused baseApi RTK Query scaffold

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { baseApi } from './api/baseApi';
 import communicationReducer from './slices/communicationSlice';
 import pairingReducer from './slices/pairingSlice';
 import uiReducer from './slices/uiSlice';
 
 export const store = configureStore({
   reducer: {
-    [baseApi.reducerPath]: baseApi.reducer,
     communication: communicationReducer,
     pairing: pairingReducer,
     ui: uiReducer,
@@ -15,15 +12,11 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        // Ignore these action types for non-serializable values
         ignoredActions: ['communication/setCurrentIcons'],
       },
-    }).concat(baseApi.middleware),
+    }),
   devTools: process.env.NODE_ENV !== 'production',
 });
-
-// Enable refetchOnFocus/refetchOnReconnect behaviors
-setupListeners(store.dispatch);
 
 // Infer types from the store
 export type RootState = ReturnType<typeof store.getState>;
